ff159985d23ca41a5f478273a5f2f1c18473b217
perry <email@email.com>
2022-04-09 11:33:31 +0900

    カテゴリ名を追加

--- components/GameCategoryDisplay.tsx
+++ components/GameCategoryDisplay.tsx
@@ -3,14 +3,17 @@ import { NextPage } from 'next'
 import Link from "next/link";
 // interfaces
 import { IGameItem } from '../interfaces/IGameItem';
+import { GameCategories } from './../interfaces/GameCategories'
 // style
 import styles from '../styles/profile.module.scss';
+import { prependOnceListener } from 'process';
 
 interface Props {
   gameList: IGameItem[];
+  gameCategoryName: string;
 }
 
-const Profile: NextPage<Props> = ({gameList}) => {
+const Profile: NextPage<Props> = ({gameList, gameCategoryName}) => {
   const displayTheData = (gameData: IGameItem[]) => {
     return (
       gameData.map((game: IGameItem, index: number) => 
@@ -27,7 +30,7 @@ const Profile: NextPage<Props> = ({gameList}) => {
   }
   return (
     <section className={styles.gameSection}>
-      <h2 className={styles.profileTitle}>Currently Playing</h2>
+      <h2 className={styles.profileTitle}>{gameCategoryName}</h2>
       <ul className={styles.gameList}>
         {
           gameList.length ? displayTheData(gameList) : 'no data'
